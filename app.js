9fc46cf63851f4c0a3a8a38c4bb196e34de8cbbd
g4n1d <jonatanciencias@gmail.com>
2024-01-11 11:51:36 -0500

    Add variables and selectors for input fields and buttons

---
+++ app.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById('inputPalabras');
+const palabras = document.getElementById('inputPalabras');
+const btnCocinar = document.getElementById('btnCocinar');
+const anchoSelect = document.getElementById('ancho');
+const largoSelect = document.getElementById('largo');
+
